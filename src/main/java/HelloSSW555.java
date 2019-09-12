518a206602af2d1604f21853347a772e5eec82c5
Haoxuan Li <kslup7@gmail.com>
2019-09-12 16:32:27 -0400

    create the base project
    version 0.1   create a new project

---
+++ src/main/java/HelloSSW555.java
@@ -0,0 +1,9 @@
+/**
+ * @author HaoxuanLi  Github:bestksl
+ * @version created date：2019-09-12 16:31
+ */
+public class HelloSSW555 {
+    public static void main(String[] args) {
+        System.out.println("hello SSW555!");
+    }
+}
