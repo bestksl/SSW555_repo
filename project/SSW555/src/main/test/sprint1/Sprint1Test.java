9f5dda72b4ebda4a29b5d5a4b80e91444d755f23
Haoxuan Li <kslup7@gmail.com>
2019-09-28 19:02:05 -0400

    fix

---
+++ project/SSW555/src/main/test/sprint1/Sprint1Test.java
@@ -0,0 +1,18 @@
+package sprint1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * @author HaoxuanLi  Github:bestksl
+ * @version created date：2019-09-28 18:49
+ */
+public class Sprint1Test {
+
+    @Test
+    public void testCheckBirthBeforeDeath() {
+        
+    }
+}
